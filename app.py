8f94c1bedfde172b3926616e1f705a9a99518c60
marmercader <mar.mercader@hotmail.com>
2017-12-11 17:11:00 +0100

    Seventh version
    commit

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+  server = app.server
+    
+    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 # Import dataset
 
